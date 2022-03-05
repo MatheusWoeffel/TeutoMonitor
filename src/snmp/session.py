8616a86fde6d447f8e267cad201313fa45322e0f
Alencar da Costa <alencar.dacosta@moovin.com.br>
2022-03-05 15:46:51 -0300

    feat: add class to abstract snmp sessions

---
+++ src/snmp/session.py
@@ -0,0 +1,32 @@
+from easysnmp import Session
+
+
+class SNMPSession:
+    def __init__(self, host_ip, snmp_version=2, security_username="",
+                 privacy_password="", privacy_protocol="",
+                 auth_password="", auth_protocol="", community="public"):
+        self.host_ip = host_ip
+        self.snmp_version = snmp_version
+        self.security_username = security_username
+        self.privacy_password = privacy_password
+        self.privacy_protocol = privacy_protocol
+        self.auth_password = auth_password
+        self.auth_protocol = auth_protocol
+
+        if self.snmp_version == 2:
+            self._session = Session(
+                hostname=host_ip, community=community, version=snmp_version)
+        elif self.snmp_version == 3:
+            self._session = Session(
+                hostname=host_ip, community=community, version=snmp_version)
+        else:
+            self._session = None
+
+    def session(self):
+        return self._session
+
+    def get_instance_name(self):
+        return self.session().get('sysName.0')
+
+    def get_interfaces_count(self):
+        return int(self.session().get('ifNumber.0').value)
