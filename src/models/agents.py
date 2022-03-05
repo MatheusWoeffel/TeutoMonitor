8904c53a764a1990313511041631becc7a0a43ed
Alencar da Costa <alencar.dacosta@moovin.com.br>
2022-03-05 14:36:20 -0300

    feat: add agents table

---
+++ src/models/agents.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, DateTime
+import datetime
+from models.base import Base
+
+
+class Agent(Base):
+    __tablename__ = 'Agents'
+    id = Column(Integer, primary_key=True)
+    host_ip = Column(String, nullable=False)
+    snmp_version = Column(Integer, nullable=False)
+    security_username = Column(String)
+    privacy_password = Column(String)
+    privacy_protocol = Column(String)
+    auth_password = Column(String)
+    auth_protocol = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+def __repr__(self):
+    return "<(Agent id='%i', host_ip=%s, snmp_version=%s, security_username=%s, privacy_password=%s, privacy_protocol=%s, auth_password=%s, auth_protocol=%s, timestamp=%s)>" % (
+        self.id, self.host_ip, self.snmp_version, self.security_username, self.privacy_password, self.privacy_protocol, self.auth_password, self.auth_protocol, self.timestamp)
