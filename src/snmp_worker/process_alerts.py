ac4bae5a7b24f03d2f87715d378830186c8793d4
Matheus Woeffel <matheuswoeffel@gmail.com>
2022-03-06 23:08:41 -0300

    feat: process alerts for all agents

--- src/snmp_worker/process_alerts.py
+++ src/snmp_worker/process_alerts.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from paramiko import Agent
+
 from models.alert import Alert
 from models.measurements import Measurement
 from snmp_worker.send_notification import send_notification
@@ -11,29 +13,35 @@ last_processed_time = datetime.now()
 def process_alerts(session):
     global last_processed_time
     alerts = session.query(Alert).all()
+    agents = session.query(Agent).all()
 
-    for alert in alerts:
-        newer_measurement = session.query(Measurement).filter(
-            Measurement.timestamp > last_processed_time
-        ).filter(
-            Measurement.metric == alert.metric
-        ).order_by(
-            Measurement.timestamp.desc()
-        ).first()
+    for agent in agents:
+        for alert in alerts:
+            newer_measurement = session.query(Measurement).filter(
+                Measurement.timestamp > last_processed_time
+            ).filter(
+                Measurement.metric == alert.metric
+            ).filter(
+                Measurement.agent_id == agent.agent_id
+            ).order_by(
+                Measurement.timestamp.desc()
+            ).first()
 
-        last_measurement = session.query(Measurement).filter(
-            Measurement.timestamp < last_processed_time
-        ).filter(
-            Measurement.metric == alert.metric
-        ).order_by(
-            Measurement.timestamp.desc()
-        ).first()
-        last_processed_time = datetime.utcnow()
-        if last_measurement is None or newer_measurement is None:
-            continue
+            last_measurement = session.query(Measurement).filter(
+                Measurement.timestamp < last_processed_time
+            ).filter(
+                Measurement.metric == alert.metric
+            ).filter(
+                Measurement.agent_id == agent.agent_id
+            ).order_by(
+                Measurement.timestamp.desc()
+            ).first()
+            last_processed_time = datetime.utcnow()
+            if last_measurement is None or newer_measurement is None:
+                continue
 
-        increase_in_value = newer_measurement.value - last_measurement.value
-        if increase_in_value > alert.increase_threshold:
-            title = f'ALERT - {alert.metric} increased more than {alert.increase_threshold}'
-            message = f'Measurement has increased by {increase_in_value} at {newer_measurement.timestamp}'
-            send_notification(title, message)
+            increase_in_value = newer_measurement.value - last_measurement.value
+            if increase_in_value > alert.increase_threshold:
+                title = f'ALERT @{agent.host_ip} - {alert.metric} increased more than {alert.increase_threshold}'
+                message = f'Measurement has increased by {increase_in_value} at {newer_measurement.timestamp}.'
+                send_notification(title, message)
